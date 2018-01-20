be1e72632c01aeb5acea4098c9223b88186a64a4
Andrew <hello@ajuk.co>
2018-01-20 23:34:50 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,14 @@
-// module.exports = {
+module.exports = {
+  var consumer_key = process.env.consumer_key;
+  var consumer_secret = process.env.consumer_secret;
+  var access_token_key = process.env.access_token_key;
+  var access_token_secret = process.env.access_token_secret;
+  console.log(process.env)
+}
+
+console.log(process.env)
+
 //   consumer_key: 'yDIC9GRVTlgOnyEcdZzI9tjt1',
 //   consumer_secret: '50wnxrad1CYgTaCADcjCWXxtD0AM0on5YEgGr6e5LzMBrM8eOG',
 //   access_token_key: '923201098227306496-wMbQK55OGqB0HGUkCWzlySXf9jqA4pG',
 //   access_token_secret: 'XTqUAZaIXEmSAo5LFFo5rUFs9x16KwnWAOp6qwRpMzn78'
-  // var consumer_key = process.env.consumer_key;
-  // var consumer_secret = process.env.consumer_secret;
-  // var access_token_key = process.env.access_token_key;
-  // var access_token_secret = process.env.access_token_secret;
-// }
-
-var consumer_key = process.env.consumer_key;
-var consumer_secret = process.env.consumer_secret;
-var access_token_key = process.env.access_token_key;
-var access_token_secret = process.env.access_token_secret;
