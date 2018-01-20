1510bb65f20a89e9718eb47f13b829cc2329e14a
Andrew <hello@ajuk.co>
2018-01-20 23:55:24 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
-  var consumer_key = process.env.consumer_key;
-  var consumer_secret = process.env.consumer_secret;
-  var access_token_key = process.env.access_token_key;
-  var access_token_secret = process.env.access_token_secret;
-  console.log(process.env)
+//   var consumer_key = process.env.consumer_key;
+//   var consumer_secret = process.env.consumer_secret;
+//   var access_token_key = process.env.access_token_key;
+//   var access_token_secret = process.env.access_token_secret;
+//   console.log(process.env)
 
 
 //console.log(process.env)
@@ -12,3 +12,11 @@
 //   consumer_secret: '50wnxrad1CYgTaCADcjCWXxtD0AM0on5YEgGr6e5LzMBrM8eOG',
 //   access_token_key: '923201098227306496-wMbQK55OGqB0HGUkCWzlySXf9jqA4pG',
 //   access_token_secret: 'XTqUAZaIXEmSAo5LFFo5rUFs9x16KwnWAOp6qwRpMzn78'
+
+
+module.exports = {
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret
+}
